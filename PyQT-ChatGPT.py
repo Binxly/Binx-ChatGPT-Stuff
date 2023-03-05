db4bff7fb7bd83207773b0343ab40c55aee5af04
Zackary Binx <59242014+Binxly@users.noreply.github.com>
2023-03-05 10:07:35 -0500

    Update PyQT-ChatGPT.py

--- PyQT-ChatGPT.py
+++ PyQT-ChatGPT.py
@@ -1,78 +1,79 @@
 import openai
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore, QtGui
 
 openai.api_key = ""
 
 class ChatBotWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("GPT Assistant")
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("ChatGPT Assistant")
+        self.setStyleSheet("background-color: #222222; color: #ffffff;")
         self.layout = QtWidgets.QVBoxLayout()
-
-        # OpenAI API Credentials Parse
         self.api_key_label = QtWidgets.QLabel("OpenAI API Key:")
+        self.api_key_label.setStyleSheet("color: #ffffff;")
         self.layout.addWidget(self.api_key_label)
         self.api_key_input = QtWidgets.QLineEdit()
         self.api_key_input.setText(openai.api_key)
         self.api_key_input.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.api_key_input.setStyleSheet("background-color: #333333; color: #ffffff;")
         self.layout.addWidget(self.api_key_input)
-
         self.apply_button = QtWidgets.QPushButton("Apply")
+        self.apply_button.setStyleSheet("background-color: #333333; color: #ffffff;")
         self.apply_button.clicked.connect(self.apply_api_key)
         self.layout.addWidget(self.apply_button)
-
-        # Chat History
-        self.message_input = QtWidgets.QLineEdit()
+        self.message_input = QtWidgets.QTextEdit()
+        self.message_input.setStyleSheet("background-color: #333333; color: #ffffff;")
         self.layout.addWidget(self.message_input)
-        self.message_input.returnPressed.connect(self.send_message)
+        self.message_input.textChanged.connect(self.update_send_button)
         self.send_button = QtWidgets.QPushButton("Send")
+        self.send_button.setStyleSheet("background-color: #333333; color: #ffffff;")
+        self.send_button.setDisabled(True)
         self.send_button.clicked.connect(self.send_message)
         self.layout.addWidget(self.send_button)
-
         self.chat_history = QtWidgets.QTextEdit()
-        self.chat_history.setDisabled(True)
+        self.chat_history.setStyleSheet("background-color: #333333; color: #ffffff;")
+        self.chat_history.setReadOnly(True)
+        self.chat_history.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse|QtCore.Qt.TextSelectableByKeyboard)
         self.layout.addWidget(self.chat_history)
-
-        # Clear Chat History
         self.clear_button = QtWidgets.QPushButton("Clear")
+        self.clear_button.setStyleSheet("background-color: #333333; color: #ffffff;")
         self.clear_button.clicked.connect(self.clear_history)
         self.layout.addWidget(self.clear_button)
-
-        # Set Chat Messages
         self.messages = []
         self.setLayout(self.layout)
 
+    def clear_history(self):
+        self.chat_history.clear()
+        self.messages = []
+
     def apply_api_key(self):
-        # Apply OpenAI API Key
         openai.api_key = self.api_key_input.text()
 
-    def send_message(self):
-        # Send Message to OpenAI API and display response in chat history
-        prompt = self.message_input.text()
-        message_user = {"role": "user", "content": prompt}
-        self.messages.append(message_user)
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=self.messages,
-        max_tokens=1024,
-        temperature=0
-    )
+    def update_send_button(self):
+        if self.message_input.toPlainText().strip():
+            self.send_button.setEnabled(True)
+        else:
+            self.send_button.setDisabled(True)
 
+    def send_message(self):
+        prompt = self.message_input.toPlainText()
+        lines = prompt.split('\n')
+        messages = [{"role": "user", "content": line} for line in lines]
+        self.messages.extend(messages)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.messages,
+            max_tokens=1024,
+            temperature=0
+        )
         message_GPT = {"role": "assistant", "content": response.choices[0].message.content}
         self.messages.append(message_GPT)
-
         self.chat_history.setEnabled(True)
         self.chat_history.append(f"\n\nYou: {prompt}\n\nAssistant: {response.choices[0].message.content}")
-        self.chat_history.setDisabled(True)
-
+        self.chat_history.setReadOnly(True)
+        self.chat_history.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse|QtCore.Qt.TextSelectableByKeyboard)
         self.message_input.clear()
-
-    def clear_history(self):
-        # Clear Chat History
-        self.chat_history.setEnabled(True)
-        self.chat_history.clear()
-        self.chat_history.setDisabled(True)
-        self.messages = []
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
